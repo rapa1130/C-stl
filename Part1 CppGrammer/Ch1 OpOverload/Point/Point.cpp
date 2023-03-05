7d1e684d1b67a95f7a49981f8457df2f6b57cad3
안병헌 <rapa1130@naver.com>
2023-03-05 11:34:02 +0900

    Feat: PointClass Update
    Operator overloading which is about PointClass is added

---
+++ Part1 CppGrammer/Ch1 OpOverload/Point/Point.cpp
@@ -0,0 +1,80 @@
+#include"Point.h"
+#include<iostream>
+#include<stdlib.h>
+
+namespace PointNS{
+    Point::Point():x(0),y(0){}
+    Point::Point(int x,int y)
+    :x(x),y(y){}
+    
+void Point::Print()const{
+    using std::cout;
+    using std::endl;
+    cout<<"{"<<x<<", "<<y<<"}"<<endl;
+}
+
+int Point::GetX() const{
+    return this->x;
+}
+
+int Point::GetY() const{
+    return this->y;
+}
+void Point::SetX(int x){
+    this->x=x;
+}
+void Point::SetY(int y){
+    this->y=y;
+}
+const Point& Point::operator++(){
+    this->x++;
+    this->y++;
+    return *this;
+}
+const Point Point::operator++(int){
+    Point ret(x,y);
+    this->x++;
+    this->y++;
+    return ret;
+}
+
+const Point& Point::operator--(){
+    x--;
+    y--;
+    return *this;
+}
+const Point Point::operator--(int){
+    Point ret(x,y);
+    x--;
+    y--;
+    return ret;
+}
+const bool Point::operator==(const Point& right) const{
+    return right.x==this->x && right.y == this->y;
+}
+const bool Point::operator!=(const Point& right) const{
+    return right.x!=x || right.y != y;
+}
+Point operator-(const Point& left,const Point& right){
+    return Point(left.GetX()-right.GetX(),left.GetY()-right.GetY());
+}
+Point operator+(const Point& left,const Point& right){
+    return Point(left.x+right.x,left.y+right.y);
+}
+const int Point::operator[](int index){
+    switch(index){
+        case 1:
+            return x;
+        break;
+        case 2:
+            return y;
+        break;
+        default:
+        exit(1);
+        break;
+    }
+}
+Point::operator int() const{
+    return x;
+}
+}
---
+++ Part1 CppGrammer/Ch1 OpOverload/Point/Point.h
@@ -0,0 +1,35 @@
+#ifndef POINT_H
+#define POINT_H
+
+namespace PointNS{
+
+class Point{
+    public:
+    Point();
+    Point(int x,int y);
+    
+    void Print()const;
+    const Point& operator++(); //Prefix
+    const Point operator++(int); //Postfix
+    const Point& operator--();
+    const Point operator--(int);
+    const bool operator==(const Point& right) const;
+    const bool operator!=(const Point& right) const;
+    friend Point operator+(const Point& left,const Point& right);
+    const int operator[](int index);
+    operator int() const;
+    int GetX() const;
+    int GetY() const;
+    void SetX(int x);
+    void SetY(int y);
+
+    private:
+    int x,y;
+};
+    Point operator-(const Point& left,const Point& right);
+
+}
+
+#include"Point.cpp"
+
+#endif
